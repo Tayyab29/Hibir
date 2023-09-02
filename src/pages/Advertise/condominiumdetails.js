1e95bb4c4ca4e5f41259d22c7065dc1560929fa7
Ahmed Sherwani <ahmed.sherwani@90stech.com>
2023-09-02 20:05:39 +0500

    Hibir---Condominium Page

--- src/pages/Advertise/condominiumdetails.js
+++ src/pages/Advertise/condominiumdetails.js
@@ -1,11 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import {
-  BsChevronRight,
-  BsInfoCircle,
-  BsPlus,
-  BsPlusCircle,
-} from "react-icons/bs";
+import { BsChevronRight, BsInfoCircle, BsPlus } from "react-icons/bs";
 import { useDropzone } from "react-dropzone";
 
 const CondominiumDetails = () => {
@@ -21,6 +16,22 @@ const CondominiumDetails = () => {
     { id: 8, label: "Air Condition", checked: false },
     // ... Add more checkbox data objects
   ];
+  // emenitiescheckboxes
+  const emenitiescheckboxesData = [
+    { id: 1, label: "Furnished", checked: false },
+    { id: 2, label: "WheelChair Accessible", checked: false },
+    { id: 3, label: "Elevator", checked: false },
+    { id: 4, label: "No Smoking", checked: false },
+    { id: 5, label: "AC", checked: false },
+    { id: 6, label: "Storage", checked: false },
+    { id: 7, label: "Loft", checked: false },
+    { id: 8, label: "Fitness Center", checked: false },
+    { id: 9, label: "Fireplace ", checked: false },
+    { id: 10, label: "Gated Entry ", checked: false },
+    { id: 11, label: "Dishwasher ", checked: false },
+    { id: 12, label: "Swimming Pool", checked: false },
+    // ... Add more checkbox data objects
+  ];
   //   const [selectedDate, setSelectedDate] = useState("");
 
   //   const handleDateChange = (event) => {
@@ -30,8 +41,11 @@ const CondominiumDetails = () => {
 
   const [selectedImage, setSelectedImage] = useState(null);
   //checkbox state
-
   const [checkboxes, setCheckboxes] = useState(checkboxData);
+  //Emenities Checkboxes
+  const [emenitiescheckboxes, setEmenitiescheckboxes] = useState(
+    emenitiescheckboxesData
+  );
 
   const onDrop = (acceptedFiles) => {
     // Assuming you want to handle only one image
@@ -47,6 +61,16 @@ const CondominiumDetails = () => {
   //check box
   const handleCheckboxChange = (id) => {
     setCheckboxes((prevCheckboxes) =>
+      prevCheckboxes.map((checkbox) =>
+        checkbox.id === id
+          ? { ...checkbox, checked: !checkbox.checked }
+          : checkbox
+      )
+    );
+  };
+  // emenitiescheckboxes
+  const handleemenitiescheckboxesChange = (id) => {
+    setEmenitiescheckboxes((prevCheckboxes) =>
       prevCheckboxes.map((checkbox) =>
         checkbox.id === id
           ? { ...checkbox, checked: !checkbox.checked }
@@ -267,16 +291,66 @@ const CondominiumDetails = () => {
                 <b>Rent Specials</b>
               </h3>
             </div>
-            {/* {checkboxes.map((checkbox) => (
-              <div key={checkbox.id} className="col-md-3 col-6 mb-3">
-                <input
-                  type="checkbox"
-                  checked={checkbox.checked}
-                  onChange={() => handleCheckboxChange(checkbox.id)}
-                />
-                <span>{checkbox.label}</span>
+            <div className="col-12 col-md-4">
+              <div className="form-group">
+                <div className="form-group">
+                  <label>
+                    <b>Rest Special Tilte</b>
+                  </label>
+                  <div>
+                    <input
+                      type="text"
+                      className="condominium_input"
+                      placeholder=""
+                    />
+                  </div>
+                </div>
               </div>
-            ))} */}
+            </div>
+            <div className="col-12 col-md-4">
+              <div className="form-group">
+                <div className="form-group">
+                  <label>
+                    <b>Rest Special Start</b>
+                  </label>
+                  <div>
+                    <input
+                      type="date"
+                      className="condominium_input"
+                      placeholder=""
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-md-4">
+              <div className="form-group">
+                <div className="form-group">
+                  <label>
+                    <b>Rest Special End</b>
+                  </label>
+                  <div>
+                    <input
+                      type="date"
+                      className="condominium_input"
+                      placeholder=""
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-12">
+              <div className="form-group">
+                <label>
+                  <b>Rest Special Description(Optional)</b>
+                </label>
+                <textarea
+                  className="text_area"
+                  rows="5"
+                  placeholder=""
+                ></textarea>
+              </div>
+            </div>
           </div>
         </div>
       </section>
@@ -298,6 +372,221 @@ const CondominiumDetails = () => {
                 <span>{checkbox.label}</span>
               </div>
             ))}
+          </div>
+        </div>
+      </section>
+      <section className="pt-3">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 pb-3">
+              <h3>
+                <b>Property Emenities</b>
+              </h3>
+            </div>
+            <div className="col-12 col-md-4 mb-3">
+              <div className="form-group">
+                <label>
+                  <b>Pets Allowed</b>
+                </label>
+                <div>
+                  <select className="condominium_input">
+                    <option value="0">Pets Allowed</option>
+                    <option value="1">Pets Allowed</option>
+                    <option value="2">Pets Allowed</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-md-4 mb-3">
+              <div className="form-group">
+                <label>
+                  <b>Laundry Type</b>
+                </label>
+                <div>
+                  <select className="condominium_input">
+                    <option value="0">Laundry Type</option>
+                    <option value="1">Laundry Type</option>
+                    <option value="2">Laundry Type</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-md-4 mb-3">
+              <div className="form-group">
+                <label>
+                  <b>Parking Type</b>
+                </label>
+                <div>
+                  <select className="condominium_input">
+                    <option value="0">Parking Type</option>
+                    <option value="1">Parking Type</option>
+                    <option value="2">Parking Type</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            {emenitiescheckboxes.map((checkbox) => (
+              <div key={checkbox.id} className="col-md-3 col-6 mb-3">
+                <input
+                  type="checkbox"
+                  checked={checkbox.checked}
+                  onChange={() => handleemenitiescheckboxesChange(checkbox.id)}
+                />
+                <span>{checkbox.label}</span>
+              </div>
+            ))}
+          </div>
+          <hr></hr>
+        </div>
+      </section>
+
+      <section className="pt-3">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 pb-3">
+              <h3>
+                <b>Contact Info</b>
+              </h3>
+            </div>
+            <div className="col-12 pb-3">
+              <div className="condominim_radio_button">
+                <div className="checkbox_class">
+                  <label className="container_radio">
+                    I am an Owner
+                    <input
+                      type="radio"
+                      checked="checked"
+                      name="radio"
+                      value="owner"
+                    />
+                    <span className="checkmark"></span>
+                  </label>
+                  <label className="container_radio">
+                    I am an Agent/Broker
+                    <input type="radio" name="radio" value="agentbroker" />
+                    <span className="checkmark"></span>
+                  </label>
+                  <label className="container_radio">
+                    I am an Property Manager
+                    <input type="radio" name="radio" value="propertymanager" />
+                    <span className="checkmark"></span>
+                  </label>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-md-4 mb-3">
+              <div className="form-group">
+                <label>
+                  <b>First Name </b>
+                </label>
+                <div>
+                  <input
+                    className="condominium_input"
+                    placeholder="Jhon"
+                    type="text"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-md-4 mb-3">
+              <div className="form-group">
+                <label>
+                  <b>Last Name </b>
+                </label>
+                <div>
+                  <input
+                    className="condominium_input"
+                    placeholder="Smith"
+                    type="text"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-md-4 mb-3">
+              <div className="form-group">
+                <label>
+                  <b>Contact Preferences</b>
+                </label>
+                <div>
+                  <select className="condominium_input">
+                    <option value="0">Select</option>
+                    <option value="1">Contact Preferences</option>
+                    <option value="2">Contact Preferences</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-md-4 mb-3">
+              <div className="form-group">
+                <label>
+                  <b>Email Address</b>
+                </label>
+                <div>
+                  <input
+                    className="condominium_input"
+                    type="email"
+                    placeholder="jhonsmith@gmail.com"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-md-4 mb-3">
+              <div className="form-group">
+                <label>
+                  <b>Phone</b>
+                </label>
+                <div>
+                  <input
+                    className="condominium_input"
+                    type="number"
+                    placeholder="(123) 456 789"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 pb-2">
+            <div className="checkbox_hide_span">
+              <input type="checkbox" /> <span>Hide my Name on Hibir.com</span>
+            </div>
+          </div>
+          <hr></hr>
+        </div>
+      </section>
+      <section className="pt-2">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="checkbox_hide_bottom">
+                <input type="checkbox" />
+                <span>
+                  There are many variations of passages of Lorem Ipsum
+                  available, but the majority have suffered alteration in some
+                  form, by injected humour, or randomised words which don't look
+                  even slightly believable. If you are going to use a passage of
+                  Lorem Ipsum, you need to be sure there isn't anything
+                  embarrassing hidden in the middle of text.
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="pt-3 mb-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 col-12"></div>
+            <div className="col-md-4 col-12">
+              <div className="save_publish">
+                <button className="condominium_save" type="button">
+                  Save
+                </button>
+                <button className="condominium_publish" type="button">
+                  Publish
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </section>
