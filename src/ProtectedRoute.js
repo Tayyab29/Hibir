e34d838342edb93674c65f84df6da17160c41f45
Tayyab29 <xhahg32@gmail.com>
2023-09-24 01:09:55 +0500

    docker files added

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -1,16 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Redirect, Route, useHistory } from "react-router-dom";
 
-export const ProtectedRoute = ({
-  required_rights,
-  optional_rights,
-  parent_path,
-  children,
-  ...rest
-}) => {
+export const ProtectedRoute = ({ children, ...rest }) => {
   // Constants
   const token = localStorage.getItem("accessToken");
-  console.log({ ...rest });
 
   return <Route {...rest} render={({ location }) => (token ? children : <Redirect to="/" />)} />;
 };
