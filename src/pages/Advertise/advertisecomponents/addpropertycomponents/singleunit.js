63d84c8ab76764ebe0bcabda7aaeb2da450c9b2c
Sherwani0790 <sherwani0790@gmail.com>
2023-10-22 13:41:24 -0700

    Hibir, add validation of formik, also add error css, add select custom dropdown

--- src/pages/Advertise/advertisecomponents/addpropertycomponents/singleunit.js
+++ src/pages/Advertise/advertisecomponents/addpropertycomponents/singleunit.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom"; // version 5.2.0
 import { mainViewState, onFormAdvertiseDataChange } from "../../../../redux/main-view";
 import CustomInput from "../../../../ui-components/custominput";
+import CustomDropdown from "../../../../ui-components/customdropdown";
 const SingleUnit = () => {
   const history = useHistory();
 
@@ -34,7 +35,22 @@ const SingleUnit = () => {
     dispatch(onFormAdvertiseDataChange(localForm));
     history.push("./condominiumdetails");
   };
-
+  const [dropdownOptions, setDropdownOptions] = useState([
+    { value: 'option0', label: 'Select a number of beds' },
+    { value: 'option1', label: '0.5' },
+    { value: 'option2', label: '1' },
+    { value: 'option3', label: '1.5' },
+    { value: 'option4', label: '2' },
+    { value: 'option5', label: '2.5' },
+    { value: 'option6', label: '3' },
+    { value: 'option7', label: '3.5' },
+    { value: 'option8', label: '4' },
+    { value: 'option9', label: '4.5' },
+    { value: 'option10', label: '5' },
+    { value: 'option11', label: '5.5' },
+    { value: 'option12', label: '6' },
+    { value: 'option13', label: '6.5' },
+  ]);
   return (
     <>
       <div className="row mt-4">
@@ -105,13 +121,20 @@ const SingleUnit = () => {
                 <b>Baths</b>
               </label>
               <div>
-                <select
+                <CustomDropdown
+                  className="single_unit_input"
+                  id="propertyBaths"
+                  name="propertyBaths"
+                  onChange={inputHandler}
+                  options={dropdownOptions}
+                />
+                {/* <select
                   className="single_unit_input"
                   id="propertyBaths"
                   name="propertyBaths"
                   onChange={inputHandler}
                 >
-                  <option value="">Select a number of beds</option> {/* Placeholder */}
+                  <option value="">Select a number of beds</option>
                   <option value="0.5">0.5</option>
                   <option value="1">1</option>
                   <option value="1.5">1.5</option>
@@ -125,7 +148,7 @@ const SingleUnit = () => {
                   <option value="5.5">5.5</option>
                   <option value="6">6</option>
                   <option value="6.5">6.5</option>
-                </select>
+                </select> */}
               </div>
             </div>
           </div>
--- src/ui-components/customdropdown/index.js
+++ src/ui-components/customdropdown/index.js
@@ -5,8 +5,14 @@ const CustomDropdown = (props) => {
     const { value, options, onChange, optionLabel, placeholder, id, name, className } = props
     return (
         <>
-
-            <Dropdown className={className} value={value} options={options} onChange={onChange} optionLabel={optionLabel} placeholder={placeholder} id={id} name={name} />
+            <select className={className} id={id} name={name} onChange={onChange}>
+                {options.map((option, index) => (
+                    <option key={index} value={option.value}>
+                        {option.label}
+                    </option>
+                ))}
+            </select>
+            {/* <Dropdown className={className} value={value} options={options} onChange={onChange} optionLabel={optionLabel} placeholder={placeholder} id={id} name={name} /> */}
         </>
     )
 }
